a077073f2dd47f0a8264ee2d496471d20aa43a7e
chimebukanian <chimebukaanyanwu@gmail.com>
2022-09-17 17:02:50 -0700

    fixes

--- models/amenity.py
+++ models/amenity.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from os import getenv
-
-storage = getenv('HBNB_TYPE_STORAGE')
+from models.base_model import BaseModel
 
 
-
-class Amenity(BaseModel, Base):
-    __tablename__ = 'amenities'
-    if storage == 'db':
-        name = Column(String(128), nullable=False)
-    else:
-        name = ""
+class Amenity(BaseModel):
+    name = ""
